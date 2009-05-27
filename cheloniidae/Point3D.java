26b2beddc6e93879963f876e42cd53e83413ba98
Spencer Tipping <spencertipping@danneskjold.aerioconnect.net>
2009-05-27 10:00:53 -0700

    Considering scrapping the new Scala version and just editing the Java one to be more suited to what I want to do...

--- cheloniidae/Point3D.java
+++ cheloniidae/Point3D.java
@@ -1,162 +1,67 @@
-// vim: set ts=4 sw=4 foldmethod=marker foldminlines=1 foldlevel=0 :					// {{{
-
-//
 // This file was imported from the Rockhopper raytracer project.
-//
 // Point3D by Spencer Tipping, copyright 2006 (all rights reserved)
 // Written some time ago; licensed under the LGPL, latest version
-//
 
-//
-// Package definition
-//
+package cheloniidae;
 
-	package cheloniidae;
+import java.io.Serializable;
+
+/**
+ * This class implements a point or vector in three-dimensional space.
+ * It provides the usual set of operators, such as addition, scaled addition,
+ * multiplication by a factor, dot product, and cross product (there may
+ * be more), but does not provide more advanced operators such as rotation.
+ *
+ * @author Spencer Tipping
+ */
+
+public final class Point3D implements Serializable {
+  public double x = 0.0;
+  public double y = 0.0;
+  public double z = 0.0;
+
+  public Point3D ()                             {}
+  public Point3D (Point3D existing)             {this (existing.x, existing.y, existing.z);}
+  public Point3D (double all)                   {this (all, all, all);}
+  public Point3D (double x, double y, double z) {this.assign (x, y, z);}
+
+  public String toString () {
+    return "Point3D: <" + Double.toString (x) + ", " + Double.toString (y) + ", " + Double.toString (z) + ">";
+  }
+
+  public Point3D assign (Point3D value)                   {return this.assign (value.x, value.y, value.z);}
+  public Point3D assign (double nx, double ny, double nz) {x = nx; y = ny; z = nz; return this;}
+  public Point3D center ()                                {return this.assign (0, 0, 0);}
 
 
-//
-// Imports
-//
+  public Point3D addScaled (Point3D other, double factor) {x += other.x * factor;
+                                                           y += other.y * factor;
+                                                           z += other.z * factor;
+                                                           return this;}
 
-	import java.io.Serializable;
+  public Point3D add      (Point3D other) {x += other.x; y += other.y; z += other.z; return this;}
+  public Point3D subtract (Point3D other) {x -= other.x; y -= other.y; z -= other.z; return this;}
+  public Point3D multiply (Point3D other) {x *= other.x; y *= other.y; z *= other.z; return this;}
+  public Point3D multiply (double factor) {x *= factor;  y *= factor;  z *= factor;  return this;}
+  public Point3D divide   (Point3D other) {x /= other.x; y /= other.y; z /= other.z; return this;}
+  public Point3D divide   (double factor) {x /= factor;  y /= factor;  z /= factor;  return this;}
 
-																						// }}}
-	
-//
-// Classes
-//
+  public double dot (Point3D other) {return x * other.x + y * other.y + z * other.z;}
 
-	/**
-	 * This class implements a point or vector in three-dimensional space.
-	 * It provides the usual set of operators, such as addition, scaled addition,
-	 * multiplication by a factor, dot product, and cross product (there may
-	 * be more), but does not provide more advanced operators such as rotation.
-	 *
-	 * @author Spencer Tipping
-	 */
-	
-		public class Point3D implements Serializable {									// {{{
-			//
-			// Fields
-			//
-																						// {{{	
-				public double		X		= 0.0;
-				public double		Y		= 0.0;
-				public double		Z		= 0.0;
-																						// }}}
+  public Point3D cross (Point3D other) {
+    return new Point3D (
+        y * other.z - z * other.y,
+        z * other.x - x * other.z,
+        x * other.y - y * other.x);
+  }
 
-			//
-			// Constructors
-			//
-																						// {{{
-				public Point3D () {}
-				
-				public Point3D (double x, double y, double z) {							// {{{
-					X = x;
-					Y = y;
-					Z = z;
-				}																		// }}}
+  public double length () {return Math.sqrt (x*x + y*y + z*z);}
 
-				public Point3D (Point3D existing) {										// {{{
-					X = existing.X;
-					Y = existing.Y;
-					Z = existing.Z;
-				}																		// }}}
+  public double lengthSquared () {return x*x + y*y + z*z;}
 
-				public Point3D (double all) {											// {{{
-					X = Y = Z = all;
-				}																		// }}}
-																						// }}}
-
-			//
-			// Methods
-			//
-																						// {{{
-				public String toString () {												// {{{
-					return "Point3D: <" + Double.toString (X) + ", " + Double.toString (Y) + ", " + Double.toString (Z) + ">";
-				}																		// }}}
-	
-				public void assign (Point3D value) {									// {{{
-					X = value.X;
-					Y = value.Y;
-					Z = value.Z;
-				}																		// }}}
-
-				public void assign (double x, double y, double z) {						// {{{
-					X = x;
-					Y = y;
-					Z = z;
-				}																		// }}}
-
-				public void center () {													// {{{
-					X = Y = Z = 0.0;
-				}																		// }}}
-				
-				public void add (Point3D other) {										// {{{
-					X += other.X;
-					Y += other.Y;
-					Z += other.Z;
-				}																		// }}}
-
-				public void addScaled (Point3D other, double factor) {					// {{{
-					X += other.X * factor;
-					Y += other.Y * factor;
-					Z += other.Z * factor;
-				}																		// }}}
-
-				public void subtract (Point3D other) {									// {{{
-					X -= other.X;
-					Y -= other.Y;
-					Z -= other.Z;
-				}																		// }}}
-
-				public void multiply (Point3D other) {									// {{{
-					X *= other.X;
-					Y *= other.Y;
-					Z *= other.Z;
-				}																		// }}}
-
-				public void multiply (double factor) {									// {{{
-					X *= factor;
-					Y *= factor;
-					Z *= factor;
-				}																		// }}}
-
-				public void divide (Point3D other) {									// {{{
-					X /= other.X;
-					Y /= other.Y;
-					Z /= other.Z;
-				}																		// }}}
-
-				public void divide (double factor) {									// {{{
-					X /= factor;
-					Y /= factor;
-					Z /= factor;
-				}																		// }}}
-
-				public double dot (Point3D other) {										// {{{
-					return X * other.X + Y * other.Y + Z * other.Z;
-				}																		// }}}
-
-				public Point3D cross (Point3D other) {									// {{{
-					return new Point3D (
-						Y * other.Z - Z * other.Y,
-						Z * other.X - X * other.Z,
-						X * other.Y - Y * other.X);
-				}																		// }}}
-				
-				public double length () {												// {{{
-					return Math.sqrt (X*X + Y*Y + Z*Z);
-				}																		// }}}
-
-				public double lengthSquared () {										// {{{
-					return X*X + Y*Y + Z*Z;
-				}																		// }}}
-
-				public double distanceFrom (Point3D other) {							// {{{
-					return Math.sqrt ((X - other.X) * (X - other.X) +
-							  (Y - other.Y) * (Y - other.Y) +
-							  (Z - other.Z) * (Z - other.Z));
-				}																		// }}}
-																						// }}}
-		}																				// }}}
+  public double distanceFrom (Point3D other) {
+    return Math.sqrt ((x - other.x) * (x - other.x) +
+                      (y - other.y) * (y - other.y) +
+                      (z - other.z) * (z - other.z));
+  }
+}
