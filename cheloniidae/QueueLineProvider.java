d7db57e5ed5acc644c80aa4e2d20fbe1cadfe55b
Spencer Tipping <spencertipping@danneskjold.aerioconnect.net>
2009-06-07 00:53:21 -0700

    Fixed quicksort bugs; now there are none known.

--- cheloniidae/QueueLineProvider.java
+++ cheloniidae/QueueLineProvider.java
@@ -15,19 +15,21 @@ public class QueueLineProvider extends ArrayList<Line> implements LineProvider {
   }
 
   private final void quicksort (int a, int b) {
-    int    ia    = a + 1;
-    int    ib    = b;
-    int    pivot = (a + b) >> 1;
-    double d     = super.get (pivot).cachedDistance;
+    if (a < b) {
+      int    ia    = a + 1;
+      int    ib    = b + 1;
+      int    pivot = (a + b) >> 1;
+      double d     = super.get (pivot).cachedDistance;
 
-    swap (a, pivot);
-    while (ia < ib) {while (ia < b && super.get (ia).cachedDistance > d) ++ia;
-                     while (ib > a && super.get (ib).cachedDistance < d) --ib;
-                     if (ia < ib) swap (ia, ib);}
-    swap (a, ib);
+      swap (a, pivot);
+      while (ia < ib) {while (ia < b && super.get (ia).cachedDistance > d) ++ia;
+                       while           (super.get (--ib).cachedDistance < d);
+                       if (ia < ib) swap (ia, ib);}
+      swap (a, ib);
 
-    if (! cancelSort && a < ib - 2) quicksort (a, ib - 1);
-    if (! cancelSort && b > ib)     quicksort (ib + 1, b);
+      quicksort (a, ib - 1);
+      quicksort (ib + 1, b);
+    }
   }
 
   public final void sort (Vector center) {
