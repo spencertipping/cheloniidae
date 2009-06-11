ec3299cfc0b1dbe7047db82d10a3f6541d2d779a
Spencer Tipping <spencertipping@danneskjold.aerioconnect.net>
2009-06-10 21:39:34 -0700

    Revised the Koch curve example.

--- examples/kochcurve.java
+++ examples/kochcurve.java
@@ -1,31 +1,28 @@
-// vim: set ts=2 sw=2 :
-
 import cheloniidae.*;
+import cheloniidae.frames.*;
+
 import java.awt.*;
 
-public class kochcurve {
-	public static void main (String [] args) {
-		TurtleDrawingWindow       w = new TurtleDrawingWindow ();
-		RotationalCartesianTurtle t = new RotationalCartesianTurtle ();
+public class kochcurve extends SingleTurtleScene {
+  public static void main (String [] args) {
+    new kochcurve ();
+  }
 
-		w.add (t).setVisible (true);
-		t.lineSize (0.5).lineColor (Color.BLUE);
+  public TurtleCommand run () {
+    return repeat (3, curve (400, 5),
+                      turn (120));
+  }
 
-		for (int i = 0; i < 3; i++) {
-			curve (t, 400, 5);
-			t.turn (120);
-		}
-	}
-
-	public static void curve (RotationalCartesianTurtle t, double distance, int recursionlevel) {
-		if (recursionlevel > 0) {
-			curve (t, distance / 3.0, recursionlevel - 1);
-			t.turn (-60);
-			curve (t, distance / 3.0, recursionlevel - 1);
-			t.turn (120);
-			curve (t, distance / 3.0, recursionlevel - 1);
-			t.turn (-60);
-			curve (t, distance / 3.0, recursionlevel - 1);
-		} else t.move (distance);
-	}
+  public TurtleCommand curve (double distance, int recursionlevel) {
+    if (recursionlevel > 0) {
+      return sequence (
+        curve (distance / 3.0, recursionlevel - 1),
+        turn (-60),
+        curve (distance / 3.0, recursionlevel - 1),
+        turn (120),
+        curve (distance / 3.0, recursionlevel - 1),
+        turn (-60),
+        curve (distance / 3.0, recursionlevel - 1));
+    } else return move (distance);
+  }
 }
