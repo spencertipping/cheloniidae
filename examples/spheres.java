a97772956b4791b7d85959a69b36c9e04651723b
Spencer Tipping <spencertipping@danneskjold.aerioconnect.net>
2009-06-15 08:40:40 -0700

    Reformatted some example scenes.

--- examples/spheres.java
+++ examples/spheres.java
@@ -1,31 +1,22 @@
-// vim: set ts=2 sw=2 :
-
 import cheloniidae.*;
+import cheloniidae.frames.*;
+
 import java.awt.*;
 
-public class spheres {
-	public static void main (String [] args) {
-		TurtleDrawingWindow       w = new TurtleDrawingWindow ();
-		RotationalCartesianTurtle t = new RotationalCartesianTurtle ();
-		
-		w.add (t);
-		w.setVisible (true);
+public class spheres extends SingleTurtleScene {
+  public static void main (String[] args) {
+    new spheres ();
+  }
 
-		t.lineSize (0.1).lineColor (new Color (0.3f, 0.5f, 0.5f, 0.5f)).bodyColor (new Color (0.3f, 0.5f, 0.5f));
-		
-    for (int i = 0; i < 10; ++i) {
-      for (int j = 0; j < 35; ++j) {
-        zsphere (t, 8 - (i * 0.5));
-        t.turn (3);
-      }
-      t.turn (-30).bank (8);
-    }
-	}
+  public TurtleCommand run () {
+    return sequence (size (0.1), color (new Color (0.3f, 0.5f, 0.5f, 0.5f)), spheres (10));
+  }
 
-	public static void zsphere (RotationalCartesianTurtle t, double step) {
-		for (int i = 0; i < 72; i++) {
-			t.move (step);
-			t.pitch (5);
-		}
-	}
+  public TurtleCommand spheres (int size) {
+    return sequence (repeat (35, repeat (72, move (8 - size * 0.5), pitch (5)),
+                                 turn (3)),
+                     turn (-30),
+                     bank (8),
+                     size > 0 ? spheres (size - 1) : pass ());
+  }
 }
--- examples/spirals.java
+++ examples/spirals.java
@@ -4,24 +4,24 @@ import java.awt.*;
 public class spirals {
   public static class RTurtle extends RotationalCartesianTurtle<RTurtle> {}
 
-	public static void main (String [] args) {
-		RTurtle t1 = new RTurtle ();
-		RTurtle t2 = new RTurtle ();
-		RTurtle t3 = new RTurtle ();
+  public static void main (String [] args) {
+    RTurtle t1 = new RTurtle ();
+    RTurtle t2 = new RTurtle ();
+    RTurtle t3 = new RTurtle ();
     RTurtle t4 = new RTurtle ();
-		TurtleWindow w = new TurtleWindow ();
+    TurtleWindow w = new TurtleWindow ();
 
-		w.add (t1).add (t2).add (t3).add (t4).setVisible (true);
+    w.add (t1).add (t2).add (t3).add (t4).setVisible (true);
 
-		t1.lineSize (0.125).lineColor (new Color (0.3f, 0.5f, 0.32f, 0.5f)).position (new Vector (141, 0, 0));
-		t2.lineSize (0.125).lineColor (new Color (0.4f, 0.3f, 0.15f, 0.5f)).position (new Vector (-141, 0, 0));
-		t3.lineSize (0.125).lineColor (new Color (0.2f, 0.3f, 0.60f, 0.5f)).position (new Vector (0, 141, 0));
+    t1.lineSize (0.125).lineColor (new Color (0.3f, 0.5f, 0.32f, 0.5f)).position (new Vector (141, 0, 0));
+    t2.lineSize (0.125).lineColor (new Color (0.4f, 0.3f, 0.15f, 0.5f)).position (new Vector (-141, 0, 0));
+    t3.lineSize (0.125).lineColor (new Color (0.2f, 0.3f, 0.60f, 0.5f)).position (new Vector (0, 141, 0));
     t4.lineSize (0.125).lineColor (new Color (0.5f, 0.4f, 0.15f, 0.5f)).position (new Vector (0, -141, 0));
 
-		for (int i = 0; i < 2000; i++) {
-			t1.move (Math.sqrt (i) * 1.5).pitch (60.1);
-			t2.move (Math.sqrt (i) * 1.9).pitch (120.1);
-			t3.move (Math.sqrt (i) * 1.7).pitch (90.1);
+    for (int i = 0; i < 2000; i++) {
+      t1.move (Math.sqrt (i) * 1.5).pitch (60.1);
+      t2.move (Math.sqrt (i) * 1.9).pitch (120.1);
+      t3.move (Math.sqrt (i) * 1.7).pitch (90.1);
       if (i % 10 == 0) for (int j = 0; j < 60; ++j) t4.move (Math.sqrt (i) * 0.2).pitch (6.0);
 
       if (i % 6 == 0) t1.turn (140.2 / 60.0);
@@ -30,6 +30,6 @@ public class spirals {
       if (i % 10 == 0) t4.turn (4);
 
       if (i % 100 == 0) w.pause (100);
-		}
-	}
+    }
+  }
 }
