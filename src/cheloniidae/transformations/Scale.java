18f66460d66e6a8ffee245af478332d4e62f81d4
Spencer Tipping <spencer@spencertipping.com>
2009-09-06 16:18:19 -0600

    Added a scaling transformation. This is useful for recursive replication, which has not yet been defined.

---
+++ src/cheloniidae/transformations/Scale.java
@@ -0,0 +1,15 @@
+package cheloniidae.transformations;
+
+import cheloniidae.*;
+import cheloniidae.commands.*;
+
+public class Scale implements Transformation<TurtleCommmand> {
+  public final double factor;
+  public Scale (double _factor) {factor = _factor;}
+
+  public TurtleCommand transform (TurtleCommand c) {
+    if      (c instanceof Move) return new Move (((Move) c).value * factor);
+    else if (c instanceof Jump) return new Jump (((Jump) c).value * factor);
+    else                        return c;
+  }
+}
