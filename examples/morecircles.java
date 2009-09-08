9420507dc1a9e95f19f10bda3739c6ba6ff83a22
Spencer Tipping <spencer@spencertipping.com>
2009-09-08 14:48:02 -0600

    Minor changes to morecircles.

--- examples/morecircles.java
+++ examples/morecircles.java
@@ -6,10 +6,10 @@ public class morecircles extends SingleTurtleScene {
 
   public TurtleCommand commands () {
     return recursiveBlock ("circlestack",
-                           size (10), turn (1),
-                           pitch (-90), move (750), pitch (90),
-                           inductiveReplicator (3, sequence (pitch (-90), move (250), pitch (90), turn (3), visible (false)),
-                                                repeat (6, move (500), turn (60),
+                           pitch (-90), size (40), move (750), size (10), pitch (90),
+                           turn (1),
+                           inductiveReplicator (3, sequence (pitch (-90), move (250), pitch (90), turn (1), visible (false)),
+                                                repeat (6, move (500), turn (60), move (100),
                                                            recurse ("circlestack", 3, scale (1.0 / 3.0, true), pass ()))));
   }
 }
