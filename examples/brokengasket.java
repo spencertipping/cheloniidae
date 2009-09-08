c3b196aa2c6e25d985069e6d3187f6b82055b596
Spencer Tipping <spencer@spencertipping.com>
2009-09-08 17:11:07 -0600

    Added a new example file -- a buggy Sierpinski gasket, but it looks cool.

---
+++ examples/brokengasket.java
@@ -0,0 +1,20 @@
+import cheloniidae.*;
+import cheloniidae.frames.*;
+
+public class brokengasket extends DarkSingleTurtleScene {
+  public static void main (String[] args) {new brokengasket ();}
+
+  public TurtleCommand commands () {
+    TurtleCommand recursiveInvocation = recurse ("brokengasket", 7, scale (1.0 / 2.0, true), pass ());
+    double scale = 500;
+    return recursiveBlock ("brokengasket",
+                           size (40),
+                           recursiveInvocation, move (scale),
+                           bank (-60), turn (120), move (scale), jump (-scale), turn (-120), bank (60), turn (120),
+                           recursiveInvocation, move (scale),
+                           bank (-60), turn (120), move (scale), jump (-scale), turn (-120), bank (60), turn (120),
+                           recursiveInvocation, move (scale),
+                           bank (-60), turn (120), move (scale), turn (120), recursiveInvocation, turn (-120),
+                                                   jump (-scale), turn (-120), bank (60), turn (120));
+  }
+}
