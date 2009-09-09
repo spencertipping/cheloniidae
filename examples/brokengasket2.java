edb069f9dfc71e41639268a30aec9574b68603ae
Spencer Tipping <spencer@spencertipping.com>
2009-09-09 15:04:45 -0600

    Added a broken 4-sided gasket.

---
+++ examples/brokengasket2.java
@@ -0,0 +1,17 @@
+import cheloniidae.*;
+import cheloniidae.frames.*;
+
+public class brokengasket2 extends DarkSingleTurtleScene {
+  public static void main (String[] args) {new brokengasket2 ();}
+
+  public TurtleCommand commands () {
+    TurtleCommand recursiveInvocation = recurse ("brokengasket2", 3, scale (1.0 / 3.0, true), pass ());
+    final double scale = 500;
+    final double size  = 20;
+    return recursiveBlock ("brokengasket2",
+                           repeat (4, size (size), move (scale * 2), recursiveInvocation, size (size), move (scale), recursiveInvocation, size (size), pitch (90),
+                                      move (scale), recursiveInvocation, size (size), move (scale), recursiveInvocation, size (size), move (scale), pitch (90),
+                                      move (scale), recursiveInvocation, size (size), move (scale * 2), jump (-scale * 3),
+                                        pitch (-90), jump (-scale * 3), pitch (-90), turn (90), bank (1)));
+  }
+}
