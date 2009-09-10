9f72cd943f2fd68e1942ccb2cc9233da6c43715d
Spencer Tipping <spencer@spencertipping.com>
2009-09-09 20:02:03 -0600

    Added an example file to demonstrate text rendering.

---
+++ examples/text.java
@@ -0,0 +1,14 @@
+import cheloniidae.*;
+import cheloniidae.resources.*;
+import cheloniidae.frames.*;
+
+import java.util.*;
+
+public class text extends SingleTurtleScene {
+  public static void main (String[] args) {new text ();}
+
+  public TurtleCommand commands () {
+    BasicTextRenderer t = new BasicTextRenderer ();
+    return sequence (size (1), t.drawText ("public static void main (String[] args) {new text ();}").interspersing (sequence (pitch (15), turn (90), jump (2), turn (-84))));
+  }
+}
