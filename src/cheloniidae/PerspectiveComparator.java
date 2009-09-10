b65d10dfd91384ad08ec6f4c8d2d1b95c172b185
Spencer Tipping <spencer@spencertipping.com>
2009-09-09 21:40:57 -0600

    Added some much-needed documentation to the perspective comparator.

--- src/cheloniidae/PerspectiveComparator.java
+++ src/cheloniidae/PerspectiveComparator.java
@@ -8,6 +8,11 @@ public final class PerspectiveComparator implements Comparator<HasPerspectivePro
   public PerspectiveComparator (Viewport _v) {v = _v;}
 
   public final int compare (HasPerspectiveProjection p1, HasPerspectiveProjection p2) {
+    // A couple of notes about this. First, notice that we're comparing in reverse. This is intentional. We want to sort such that the objects farthest away
+    // from the point of view are at the beginning, since they should be rendered first. Second, notice that we never return equality. This is because a TreeSet
+    // is a proper set, not just a bag of elements. Items that compare equal belong to the same equivalence class, so they would be considered duplicates. Thus
+    // we make sure that the comparison never returns equality.
+
     return p2.depth (v) < p1.depth (v) ? -1 : 1;
   }
 }
