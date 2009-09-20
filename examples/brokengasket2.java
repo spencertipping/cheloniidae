542d35a4979062a63725a5c906eca00f4858f7ac
Spencer Tipping <spencer@spencertipping.com>
2009-09-19 23:11:16 -0600

    Turned the turtle around to provide a better initial view of the scene.

--- examples/brokengasket2.java
+++ examples/brokengasket2.java
@@ -8,10 +8,11 @@ public class brokengasket2 extends DarkSingleTurtleScene {
     TurtleCommand recursiveInvocation = recurse ("brokengasket2", 3, scale (1.0 / 3.0, true), pass ());
     final double scale = 500;
     final double size  = 20;
-    return recursiveBlock ("brokengasket2",
-                           repeat (4, size (size), move (scale * 2), recursiveInvocation, size (size), move (scale), recursiveInvocation, size (size), pitch (90),
-                                      move (scale), recursiveInvocation, size (size), move (scale), recursiveInvocation, size (size), move (scale), pitch (90),
-                                      move (scale), recursiveInvocation, size (size), move (scale * 2), jump (-scale * 3),
-                                        pitch (-90), jump (-scale * 3), pitch (-90), turn (90), bank (1)));
+    return sequence (pitch (180),
+                     recursiveBlock ("brokengasket2",
+                                     repeat (4, size (size), move (scale * 2), recursiveInvocation, size (size), move (scale), recursiveInvocation, size (size), pitch (90),
+                                                move (scale), recursiveInvocation, size (size), move (scale), recursiveInvocation, size (size), move (scale), pitch (90),
+                                                move (scale), recursiveInvocation, size (size), move (scale * 2), jump (-scale * 3),
+                                                  pitch (-90), jump (-scale * 3), pitch (-90), turn (90), bank (1))));
   }
 }
