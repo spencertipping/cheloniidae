ed33c94cf401078fd8a5f5739f2b4d977c2bf1c6
Spencer Tipping <spencer@spencertipping.com>
2009-10-04 17:01:53 -0600

    Wrote the projective predicate for the Named attribute

--- src/cheloniidae/attributes/Named.java
+++ src/cheloniidae/attributes/Named.java
@@ -3,11 +3,15 @@ package cheloniidae.attributes;
 import cheloniidae.Attribute;
 import cheloniidae.Predicate;
 
+import cheloniidae.predicates.CastableTo;
+
 public class Named extends AtomicAttribute {
   public final String name;
   public Named (final String _name) {name = _name;}
   
   public Predicate<Attribute> projectivePredicate () {
-    
+    return new CastableTo<Named> (Named.class, new AtomicPredicate<Attribute> () {
+      public boolean matches (final Named value) {return value.name.equals (name);}
+    });
   }
 }
