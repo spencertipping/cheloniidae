2336da0617356632a93d3566865f8b26ed538e37
Spencer Tipping <spencer@spencertipping.com>
2009-10-04 12:25:02 -0600

    Made parameter final

--- src/cheloniidae/ImmutableTurtleState.java
+++ src/cheloniidae/ImmutableTurtleState.java
@@ -1,5 +1,5 @@
 package cheloniidae;
 
 public abstract class ImmutableTurtleState implements TurtleState, TurtleCommand {
-  public TurtleCommand map (Transformation<TurtleCommand> t) {return this;}
+  public TurtleCommand map (final Transformation<TurtleCommand> t) {return this;}
 }
