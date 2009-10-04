9a53423ad822aec407ce76bcd91ddeb8bfc44b97
Spencer Tipping <spencer@spencertipping.com>
2009-10-04 16:57:14 -0600

    Added attribute support

--- src/cheloniidae/BasicTurtle.java
+++ src/cheloniidae/BasicTurtle.java
@@ -1,10 +1,16 @@
 package cheloniidae;
 
+import java.util.TreeSet;
+import java.util.Set;
+
 public abstract class BasicTurtle<T extends BasicTurtle> extends Replicable<T> implements Turtle<T> {
+  public final Set<Attribute> attributes = new TreeSet<Attribute> ();
+
   protected TurtleWindow window = null;
 
-  public TurtleWindow window ()                           {return window;}
-  public T            window (final TurtleWindow _window) {window = _window; return (T) this;}
+  public Set<Attributes> attributes ()                       {return attributes;}
+  public TurtleWindow    window ()                           {return window;}
+  public T               window (final TurtleWindow _window) {window = _window; return (T) this;}
 
   public T run (final TurtleCommand c) {
     c.applyTo (this);
