c71d279cf26d47e4ab24cd8f11bfc79f36b006b6
Spencer Tipping <spencer@spencertipping.com>
2009-10-04 11:36:09 -0600

    Minor tweaks to example files

--- examples/donut.java
+++ examples/donut.java
@@ -3,9 +3,7 @@ import cheloniidae.frames.*;
 import java.awt.*;
 
 public class donut extends SingleTurtleScene {
-  public static void main (String[] args) {
-    new donut ();
-  }
+  public static void main (String[] args) {new donut ();}
 
   public TurtleCommand commands () {
     return sequence (size (0.5), color (new Color (0.5f, 0.65f, 0.55f, 0.5f)),
--- examples/kochcurve.java
+++ examples/kochcurve.java
@@ -4,10 +4,10 @@ import cheloniidae.frames.*;
 import java.awt.Color;
 
 public class kochcurve extends DarkSingleTurtleScene {
-  public static void main (String [] args) {new kochcurve ();}
+  public static void main (final String[] args) {new kochcurve ();}
 
   public TurtleCommand commands () {
-    TurtleCommand recursiveInvocation = recurse ("curve", 4, scale (1.0 / 3.0), move (400));
+    final TurtleCommand recursiveInvocation = recurse ("curve", 4, scale (1.0 / 3.0), move (400));
     return repeat (3,
                    recursiveBlock ("curve",
                                    copy (color (new Color (0.5f, 0.6f, 0.7f, 0.3f)),
