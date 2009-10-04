86021066642bd83e32b8f5b2496e58df00caf852
Spencer Tipping <spencer@spencertipping.com>
2009-10-04 11:36:27 -0600

    Working on defining a torus using circles. So far it's not working that well.

---
+++ examples/circletorus.java
@@ -0,0 +1,12 @@
+import cheloniidae.*;
+import cheloniidae.replicators.*;
+import cheloniidae.frames.*;
+
+public class circletorus extends SingleTurtleScene {
+  public static void main (String[] args) {new circletorus ();}
+
+  public TurtleCommand commands () {
+    return inductiveReplicator (60, sequence (jump (1), turn (6), visible (false)),
+                                jump (50), bank (210), repeat (180, move (1), turn (2)));
+  }
+}
