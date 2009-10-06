784c106d8a16634f3219cf9556b50e2db50ebc40
Spencer Tipping <spencer@spencertipping.com>
2009-10-06 00:14:52 -0600

    Added a new example file

---
+++ examples/cube.java
@@ -0,0 +1,19 @@
+import cheloniidae.*;
+import cheloniidae.frames.*;
+
+public class cube extends SingleTurtleScene {
+  public static void main (String[] args) {new cube ();}
+  
+  public TurtleCommand commands () {
+    return sequence (repeat (4,
+                             triangle (repeat (2, move (100), turn (90)), move (100)),
+                             triangle (repeat (2, move (100), turn (90)), repeat (3, move (100), turn (90))),
+                             move (100), pitch (90)),
+                     bank (90),
+                     triangle (repeat (2, move (100), turn (90)), move (100)),
+                     triangle (repeat (2, move (100), turn (90)), repeat (3, move (100), turn (90))),
+                     repeat (2, move (100), pitch (-90)),
+                     triangle (repeat (2, move (100), turn (90)), move (100)),
+                     triangle (repeat (2, move (100), turn (90)), repeat (3, move (100), turn (90))));
+  }
+}
