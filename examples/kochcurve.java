877fc81d40b2b8585318a17084e0796571e58dce
Spencer Tipping <spencer@spencertipping.com>
2009-10-05 21:06:47 -0600

    Changed recursion limit for Koch curve

--- examples/kochcurve.java
+++ examples/kochcurve.java
@@ -7,8 +7,8 @@ public class kochcurve extends DarkSingleTurtleScene {
   public static void main (final String[] args) {new kochcurve ();}
 
   public TurtleCommand commands () {
-    final TurtleCommand recursiveInvocation = recurse ("curve", 4, scale (1.0 / 3.0), move (400));
-    return repeat (3,
+    final TurtleCommand recursiveInvocation = recurse ("curve", 5, scale (1.0 / 3.0), move (400));
+    return repeat (3, size (0.125),
                    recursiveBlock ("curve",
                                    copy (color (new Color (0.5f, 0.6f, 0.7f, 0.3f)),
                                          turn (-30), move (400 * Math.sqrt (3)),
