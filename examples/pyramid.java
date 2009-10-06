b8ed9e3cf7bd52ae81c16c3b18ac1055545fcd89
Spencer Tipping <spencer@spencertipping.com>
2009-10-05 20:14:33 -0600

    Got it working with triangle() commands

--- examples/pyramid.java
+++ examples/pyramid.java
@@ -1,20 +1,14 @@
 import cheloniidae.*;
 import cheloniidae.frames.*;
 
-public class pyramid extends SingleTurtleScene<PathwiseTriangleConnector<StandardRotationalTurtle>> {
+public class pyramid extends SingleTurtleScene {
   public static void main (String[] args) {new pyramid ();}
 
-  public PathwiseTriangleConnector<StandardRotationalTurtle> createTurtle () {
-    return new PathwiseTriangleConnector<StandardRotationalTurtle> ();
-  }
-
   public TurtleCommand commands () {
-    turtle.add (new StandardRotationalTurtle ().attribute (named ("bob"))).add (new StandardRotationalTurtle ().turn (60).jump (100));
-
-    return when (turtleAttribute (named ("bob").projectivePredicate ()),
-                 move (100), turtle.emitter (),
-                 turn (30), bank (30), pitch (-90), move (100), turtle.emitter ()
-                     
-                  );
+    final StandardRotationalTurtle c1 = new StandardRotationalTurtle ();
+    final StandardRotationalTurtle c2 = new StandardRotationalTurtle ().move (100);
+    final StandardRotationalTurtle c3 = new StandardRotationalTurtle ().turn (60).move (100);
+    return sequence (triangle (c2, c3),
+                     turn (30), pitch (-60), move (100), triangle (c1, c2), triangle (c2, c3), triangle (c1, c3));
   }
 }
