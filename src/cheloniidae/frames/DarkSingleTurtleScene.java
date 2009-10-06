67d368b1c92c4f396d05a7641ecf095629aa01ad
Spencer Tipping <spencer@spencertipping.com>
2009-10-05 21:06:39 -0600

    Changed default alpha

--- src/cheloniidae/frames/DarkSingleTurtleScene.java
+++ src/cheloniidae/frames/DarkSingleTurtleScene.java
@@ -7,7 +7,7 @@ import java.awt.Color;
 public abstract class DarkSingleTurtleScene<T extends Turtle> extends SingleTurtleScene<T> {
   public DarkSingleTurtleScene initialize () {
     window.setBackground (new Color (0.05f, 0.06f, 0.08f));
-    turtle.run (color (new Color (0.8f, 0.8f, 0.9f, 0.5f)));
+    turtle.run (color (new Color (0.8f, 0.8f, 0.9f, 0.3f)));
     super.initialize ();
     return this;
   }
