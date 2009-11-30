58b9a1980d57d2cf2c30d7c0afbf556387800217
Spencer Tipping <spencer@spencertipping.com>
2009-11-29 18:22:36 -0700

    Added a new example scene

--- examples/boxes.java
+++ examples/boxes.java
@@ -11,13 +11,10 @@ public class solidgasket extends SingleTurtleScene {
                                          triangle (repeat (2, jump (100), turn (90)), repeat (3, jump (100), turn (90))));
 
     final TurtleCommand cube = copy (repeat (4, face, jump (100), pitch (90)),
-                                     bank (-90), turn (-90), face, pitch (-90), jump (-100), turn (90), jump (100),
+                                     turn (-90), pitch (90), face, pitch (-90), jump (-100), turn (90), jump (100),
                                      turn (90), pitch (90), face, visible (false));
 
-    final TurtleCommand recursiveInvocation = recurse ("solidgasket", 0, scale (1.0 / 3.0, true), cube);
-
     return sequence (color (new java.awt.Color (0.2f, 0.3f, 0.4f, 0.1f)),
-                     recursiveBlock ("solidgasket",
-                                     repeat (4, recursiveInvocation, jump (100), recursiveInvocation, turn (90))));
+                            repeat (4, cube, jump (150), cube, turn (90)));
   }
 }
