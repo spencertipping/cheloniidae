c67eaf49a6f0d954b5590961e2dd784482a3b876
Spencer Tipping <spencer@spencertipping.com>
2009-12-01 10:08:05 -0700

    Fixed the dome scene

--- examples/dome.java
+++ examples/dome.java
@@ -3,29 +3,14 @@ import cheloniidae.frames.*;
 
 import static cheloniidae.frames.CoreCommands.*;
 
-public class dome extends SingleTurtleScene<PathwiseTriangleConnector<StandardRotationalTurtle>> {
-  public static void main (String[] args) {new sphereflake ();}
-
-  public PathwiseTriangleConnector<StandardRotationalTurtle> createTurtle () {
-    return new PathwiseTriangleConnector<StandardRotationalTurtle> ();
-  }
+public class dome extends SingleTurtleScene {
+  public static void main (String[] args) {new dome ();}
 
   public TurtleCommand commands () {
-    final StandardRotationalTurtle guideTurtle        = new StandardRotationalTurtle ().attribute (named ("guide"));
-    final Predicate<Turtle>        isGuideTurtle      = turtleAttribute (named ("guide").projectivePredicate ());
-    final TurtleCommand            synchronizeTurtles = guideTurtle;
-
-    turtle.add (guideTurtle).add (new StandardRotationalTurtle ());
-
-    final double        scale  = 4;
-    final TurtleCommand sphere =
-      sequence (synchronizeTurtles,
-                when (isGuideTurtle, turn (6)),
-                repeat (30, turn (6),
-                            pitch (3),
-                            repeat (60, move (scale), triangleEmit (), pitch (6)),
-                            pitch (3)));
-
-    return sphere;
+    return bandSplitReplicator (turn (6),
+                                repeat (30, turn (6),
+                                            pitch (3),
+                                            repeat (60, move (4), triangleEmit (), pitch (6)),
+                                            pitch (3)));
   }
 }
